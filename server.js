ed4c96367cda9936f89bc6336bf27d4259bfd3f1
Shivam1504 <shivamji019@gmail.com>
2023-12-30 16:13:19 +0530

    commit

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ import mongoose from "mongoose";
 import bodyparser from "express";
 import contactRouter from './routes/contact.js';
 import cors from 'cors'
+import {config} from 'dotenv'
 
 const app = express();
 
+config({
+  path:'.env'
+})
+
 app.use(bodyparser.json());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:process.env.Frontend_URL,
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }))
 
 mongoose
-  .connect(
-    "mongodb+srv://shivamji019:FqN8LL38IRDZKKDE@volcanus.8addxe8.mongodb.net/",
+  .connect(process.env.Mongo_URL,
+    
     {
       dbName: "MERN_PROJECT_Contact_Keeper",
     }
@@ -25,6 +30,6 @@ mongoose
   app.use('/api',contactRouter); 
 
 
-const port = 3000;
+const port = process.env.Port;
 app.listen(port, () => console.log(`server is running on port ${port} `));
  
